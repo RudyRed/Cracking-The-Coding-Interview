e6297feb1a6f1e27f85df7845cebf3f204ab4313
RudyRed <willgross94@gmail.com>
2018-02-07 16:37:14 -0500

    1.2 Completed

---
+++ 01 Arrays & Strings/Check Permutation.js
@@ -0,0 +1,28 @@
+// Given two strings, write a method to decide if one is a permutation of the other.
+
+const checkIfPermutation = function (s1, s2) {
+  let obj = {};
+
+  for (let letter of s1) {
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+
+  for (let letter of s2) {
+    if (!obj[letter]) return false;
+    obj[letter]--;
+  }
+
+  for (let key in obj) {
+    if (obj[key]) return false;
+  }
+
+  return true;
+};
+
+console.log(checkIfPermutation('aaabbb', 'ababab'));
+console.log(checkIfPermutation('aaabbb', 'abababc'));
+console.log(checkIfPermutation('aaabbb', 'ababa'));
