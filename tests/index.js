d2fc2ff58d21c78a9a6ae637a1417522c1281734
RudyRed <willgross94@gmail.com>
2018-02-22 13:29:45 -0500

    Added Question Number to test file names

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,12 @@
 describe('Chapter 1: Arrays and Strings', function() {
 
-  require('./01 Arrays & Strings/Check Permutation.test.js');
-  require('./01 Arrays & Strings/isUnique.test.js');
+  require('./01 Arrays & Strings/1.1 isUnique.test.js');
+  require('./01 Arrays & Strings/1.2 Check Permutation.test.js');
 
 });
 
 describe('Chapter 2: Linked Lists', function() {
 
-  // require('./02 Linked Lists/FILE NAME HERE!!!!!!!!!.test.js');
+  // require('./02 Linked Lists/2.1 FILE NAME HERE!!!!!!!!!.test.js');
 
 });
