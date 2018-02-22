69e6819aad5857f43b514e1b516edcf088c6cd2a
RudyRed <willgross94@gmail.com>
2018-02-22 13:23:17 -0500

    Changed test header name

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
-describe('Chapter 1: Arrays and Strings', function() {
+describe('Chapter 1', function() {
 
   require('./01 Arrays & Strings/Check Permutation.test.js');
   require('./01 Arrays & Strings/isUnique.test.js');
 
 });
 
-describe('Chapter 2: Linked Lists', function() {
+describe('Chapter 2', function() {
 
   // require('./02 Linked Lists/FILE NAME HERE!!!!!!!!!.test.js');
 
