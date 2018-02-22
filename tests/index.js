517337126395f9e0f98160cd2294dae81ac7e467
RudyRed <willgross94@gmail.com>
2018-02-22 13:15:26 -0500

    Init to add testing

---
+++ tests/index.js
@@ -0,0 +1,12 @@
+describe('Chapter 1: Arrays and Strings', function() {
+
+  require('./01 Arrays & Strings/Check Permutation.test.js');
+  require('./01 Arrays & Strings/isUnique.test.js');
+
+});
+
+describe('Chapter 2: Linked Lists', function() {
+
+  // require('./02 Linked Lists/FILE NAME HERE!!!!!!!!!.test.js');
+
+});
