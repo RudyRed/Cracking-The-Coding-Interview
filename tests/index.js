3bc40b547d2668f9951309d3eae2ee20191cbad5
RudyRed <willgross94@gmail.com>
2018-02-22 13:25:36 -0500

    Changed Names back

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
-describe('Chapter 1', function() {
+describe('Chapter 1: Arrays and Strings', function() {
 
   require('./01 Arrays & Strings/Check Permutation.test.js');
   require('./01 Arrays & Strings/isUnique.test.js');
 
 });
 
-describe('Chapter 2', function() {
+describe('Chapter 2: Linked Lists', function() {
 
   // require('./02 Linked Lists/FILE NAME HERE!!!!!!!!!.test.js');
 
