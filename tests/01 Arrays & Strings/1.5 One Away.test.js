d162aa8590aa389103d7743fd0ce169a172da3e4
RudyRed <willgross94@gmail.com>
2018-02-23 13:10:21 -0500

    1.5 Init

--- tests/01 Arrays & Strings/1.5 One Away.test.js
+++ tests/01 Arrays & Strings/1.5 One Away.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-describe('oneAway', () => {
+describe('One Away', () => {
   const { oneAway } = require('../../answers/01 Arrays & Strings/1.5 One Away.js');
 
 });
