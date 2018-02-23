b1fe19c24b64e58758c8a822872b7707e3b45374
RudyRed <willgross94@gmail.com>
2018-02-23 13:05:35 -0500

    1.5 Init

---
+++ tests/01 Arrays & Strings/1.5 One Away.test.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+
+describe('oneAway', () => {
+  const { oneAway } = require('../../answers/01 Arrays & Strings/1.5 One Away.js');
+
+});
--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,7 @@ describe('Chapter 1: Arrays and Strings', function() {
   require('./01 Arrays & Strings/1.2 Check Permutation.test.js');
   require('./01 Arrays & Strings/1.3 URLify.test.js');
   require('./01 Arrays & Strings/1.4 Palindrome Permutation.test.js');
+  require('./01 Arrays & Strings/1.5 One Away.test.js');
 
 });
 
