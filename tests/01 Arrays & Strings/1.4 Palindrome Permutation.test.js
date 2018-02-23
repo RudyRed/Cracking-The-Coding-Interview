9b57a0bb9d5fda3773e91baa1ed33b3d9fa01db2
RudyRed <willgross94@gmail.com>
2018-02-23 13:10:45 -0500

    1.4 Init

--- tests/01 Arrays & Strings/1.4 Palindrome Permutation.test.js
+++ tests/01 Arrays & Strings/1.4 Palindrome Permutation.test.js
@@ -1,1 +1,6 @@
 const expect = require('chai').expect;
+
+describe('Palinrome Permutation', () => {
+  const { palinPerm } = require('../../answers/01 Arrays & Strings/1.4 Palindrome Permutation.js');
+
+});
