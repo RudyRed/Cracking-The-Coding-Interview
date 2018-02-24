431284abef2f8fb6aea9f0dac7aed41b084c7ca5
RudyRed <willgross94@gmail.com>
2018-02-23 23:22:13 -0500

    Fixed Require

--- tests/01 Arrays & Strings/1.7 Rotate Matrix.test.js
+++ tests/01 Arrays & Strings/1.7 Rotate Matrix.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
 describe('Rotate Matrix', () => {
-  const { strComp } = require('../../answers/01 Arrays & Strings/1.7 Rotate Matrix.js');
+  const { rotateMatrix } = require('../../answers/01 Arrays & Strings/1.7 Rotate Matrix.js');
 
 });
--- tests/01 Arrays & Strings/1.8 Zero Matrix.test.js
+++ tests/01 Arrays & Strings/1.8 Zero Matrix.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
 describe('Zero Matrix', () => {
-  const { strComp } = require('../../answers/01 Arrays & Strings/1.8 Zero Matrix.js');
+  const { zeroMatrix } = require('../../answers/01 Arrays & Strings/1.8 Zero Matrix.js');
 
 });
