0b4877aed57cc313d325a1ab146c6d6005faf86a
RudyRed <willgross94@gmail.com>
2018-02-24 17:10:36 -0500

    1.4 Tests Complete

--- tests/01 Arrays & Strings/1.4 Palindrome Permutation.test.js
+++ tests/01 Arrays & Strings/1.4 Palindrome Permutation.test.js
@@ -3,4 +3,41 @@ const expect = require('chai').expect;
 describe('Palinrome Permutation', () => {
   const { palinPerm } = require('../../answers/01 Arrays & Strings/1.4 Palindrome Permutation.js');
 
+  it('should return a boolean', () => {
+    expect(palinPerm('abcd')).to.be.a('boolean');
+    expect(palinPerm('rraacce')[0]).to.be.a('boolean');
+  });
+
+  it('should handle empty string', () => {
+    expect(palinPerm('')).to.be.true;
+  });
+
+  it('should return true for even length possible palindromes', () => {
+    expect(palinPerm('abcabc')).to.be.true;
+    expect(palinPerm('eekkaallii')).to.be.true;
+  });
+
+  it('should return false for even length non-palindromes', () => {
+    expect(palinPerm('abcabd')).to.be.false;
+    expect(palinPerm('(eekkaallii)')).to.be.false;
+  });
+
+  it('should return true for odd length possible palindromes', () => {
+    expect(palinPerm('rraacce')).to.be.true;
+    expect(palinPerm('tocatac')).to.be.true;
+  });
+
+  it('should return false for odd length non-palindromes', () => {
+    expect(palinPerm('rraac2e')).to.be.false;
+    expect(palinPerm('eKkaallii')).to.be.false;
+  });
+
+  it('Should handle/ignore spaces', () => {
+    expect(palinPerm(' t   o   c a t a c ')).to.be.true;
+    expect(palinPerm('  ')).to.be.true;
+    expect(palinPerm('abc abc')).to.be.true;
+    expect(palinPerm(' a  bc abw')).to.be.false;
+    expect(palinPerm('e Kka a ll ii')).to.be.false;
+  });
+
 });
