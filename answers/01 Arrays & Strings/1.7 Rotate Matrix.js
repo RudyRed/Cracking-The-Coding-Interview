e5afa7d5a82af7333be45cf935e4ab6ddd262147
RudyRed <willgross94@gmail.com>
2018-03-01 16:32:55 -0500

    1.7 Complete

--- answers/01 Arrays & Strings/1.7 Rotate Matrix.js
+++ answers/01 Arrays & Strings/1.7 Rotate Matrix.js
@@ -4,8 +4,34 @@
 
 
 const rotateMatrix = function (matrix) {
+  const n = matrix.length;
 
+  for (let startingRow = 0; startingRow < Math.floor(n / 2); startingRow++) {
+    for (let startingCol = startingRow; startingCol < n - startingRow - 1; startingCol++) {
+
+      // replace right with top
+      let pendingInsert = matrix[startingCol][n - startingRow - 1];
+      matrix[startingCol][n - startingRow - 1] = matrix[startingRow][startingCol];
+
+      // replace bottom with right
+      let tempHold = matrix[n - startingRow - 1][n - startingCol - 1];
+      matrix[n - startingRow - 1][n - startingCol - 1] = pendingInsert;
+      pendingInsert = tempHold;
+
+      // replace left with bottom
+      tempHold = matrix[n - startingCol - 1][startingRow];
+      matrix[n - startingCol - 1][startingRow] = pendingInsert;
+      pendingInsert = tempHold;
+
+      // replace top with left
+      matrix[startingRow][startingCol] = tempHold;
+
+    }
+  }
+
+  return matrix;
 };
+
 
 module.exports = {
   rotateMatrix,
