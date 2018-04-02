c9f2aee82b325f19b4c30f2428b48fd56a90acda
RudyRed <willgross94@gmail.com>
2018-04-02 15:38:38 -0400

    1.8 Completed

--- answers/01 Arrays & Strings/1.8 Zero Matrix.js
+++ answers/01 Arrays & Strings/1.8 Zero Matrix.js
@@ -3,7 +3,42 @@
 
 
 const zeroMatrix = function (matrix) {
+  const changeValue = 'CHANGE THIS ONE'
 
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (matrix[r][c]) continue;
+
+      for (let row = r + 1; row < matrix.length; row++) {
+        if (matrix[row][c] === 0) break;
+        matrix[row][c] = changeValue;
+      }
+
+      for (let row = r - 1; row >= 0; row--) {
+        if (matrix[row][c] === 0) break;
+        matrix[row][c] = changeValue;
+      }
+
+      for (let col = c + 1; col < matrix[r].length; col++) {
+        if (matrix[r][col] === 0) break;
+        matrix[r][col] = changeValue;
+      }
+
+      for (let col = c - 1; col >= 0; col--) {
+        if (matrix[r][col] === 0) break;
+        matrix[r][col] = changeValue;
+      }
+
+    }
+  }
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (matrix[r][c] === changeValue) matrix[r][c] = 0;
+    }
+  }
+
+  return matrix;
 };
 
 module.exports = {
