9a7b2680525278c244f6144ff1681b7532457a6f
RudyRed <willgross94@gmail.com>
2018-04-11 16:35:02 -0400

    2.1 Completed

--- answers/02 Linked Lists/2.1 Remove Dups.js
+++ answers/02 Linked Lists/2.1 Remove Dups.js
@@ -3,8 +3,24 @@
 // How would you solve this problem if a temporary buffer is not allowed?
 
 
-const rmDups = function (list) {
+const rmDups = function (list) { // with tempBuffer without would be O(n^2) time
+  const obj = {};
+  let prev = null;
+  let curr = list;
 
+  while (curr) {
+
+    if (obj[curr.val]) {
+        prev.next = curr.next;
+    } else {
+      obj[curr.val] = 1;
+      prev = curr;
+    }
+
+    curr = curr.next;
+  }
+
+  return list;
 };
 
 module.exports = {
