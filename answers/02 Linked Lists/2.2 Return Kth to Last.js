573e239ca3a786f770d1c31cb05d6cb5838009e8
RudyRed <willgross94@gmail.com>
2018-04-11 16:53:59 -0400

    2.2 Completed

--- answers/02 Linked Lists/2.2 Return Kth to Last.js
+++ answers/02 Linked Lists/2.2 Return Kth to Last.js
@@ -1,9 +1,25 @@
 // Return Kth to Last: Implement an algorithm to find the kth to last
 // element of a singly linked list.
 
+// k of 0 is the last node of list.
 
 const kToLast = function (list, k) {
+  if (!list) throw Error('invalid list');
+  let count = 0;
+  let curr = list;
 
+  while (curr) {
+    count++;
+    curr = curr.next;
+  }
+
+  if (count <= k) throw Error('list is not long enough');
+
+  curr = list;
+
+  for (let i = 0; i < count - 1 - k; i++) curr = curr.next;
+
+  return curr.val;
 };
 
 module.exports = {
