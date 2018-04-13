41eae60e70c3ae5b680efaadebfeb0fc75d072d4
RudyRed <willgross94@gmail.com>
2018-04-13 14:57:11 -0400

    4.9 Init, Test Added

--- tests/index.js
+++ tests/index.js
@@ -46,5 +46,6 @@ describe('Chapter 4: Trees & Graphs', () => {
   require('./04 Trees & Graphs/4.6 Successor.test.js');
   require('./04 Trees & Graphs/4.7 Build Order.test.js');
   require('./04 Trees & Graphs/4.8 First Common Ancestor.test.js');
+  require('./04 Trees & Graphs/4.9 BST Sequences.test.js');
 
 });
